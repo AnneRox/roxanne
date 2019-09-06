07fdcecedad97a99c24bc65421d2f37c6fdb1a48
Roxanne <roxanne@matise.nl>
2019-09-06 14:10:49 +0200

    wip

--- components/layout/header.vue
+++ components/layout/header.vue
@@ -1,7 +1,7 @@
 <template>
 	<header id="header" class="header">
 		<div class="header__FixedContainer">
-			<h1 class="small">🙋‍ Roxanne van den Aakster</h1>
+			<h1 class="small"><a href="mailto:rox.ann@hotmail.com"><span>Roxanne van den Aakster</span></a></h1>
 		</div>
 	</header>
 </template>
@@ -25,6 +25,56 @@
 			margin-top: 4rem;
 			@media #{$small-only} {
 				margin-top: 2rem;
+			}
+			a {
+				text-decoration: none;
+			}
+			h1 {
+				display: flex;
+				align-items: baseline;
+				cursor: pointer;
+				position: relative;
+				&:before {
+					content: '';
+					display: inline-flex;
+					height: 1.5rem;
+					width: 1.5rem;
+					margin-right: 0.375rem;
+					background-image: url('~/images/icon_hi.png');
+					background-position: center;
+					background-repeat: no-repeat;
+					background-size: contain;
+					z-index: 1;
+				}
+				span {
+					z-index: 1;
+				}
+				&:after {
+					position: absolute;
+					content: '';
+					display: flex;
+					height: 8rem;
+					width: 8rem;
+					border-radius: 50%;
+					left: -3rem;
+					top: -2rem;
+					transform: scale(0);
+					transition: ease 0.3s;
+					background-image: url('~/images/img_rox.png');
+					background-position: center;
+					background-repeat: no-repeat;
+					background-size: cover;
+				}
+				&:hover {
+					&:before {
+						background-image: url('~/images/icon_laptop.png');
+					}
+					&:after {
+						transform: scale(1);
+						left: -9rem;
+						transition: ease 0.3s;
+					}
+				}
 			}
 		}
 	}
